4790b2585b3425c139b62fa7fdbf92f432eb5582
DaleNaci <dtn22@njit.edu>
2022-12-04 05:35:22 -0500

    Updating

--- data/middleware/shipping_address_modifier.php
+++ data/middleware/shipping_address_modifier.php
@@ -2,4 +2,15 @@
     require_once("../backend/add_shipping_address.php");
 
     
+    if (isset($_POST["add-shipping-address"])) {
+        add_shipping_address(
+            $_POST["shipping-address-name"],
+            $_POST["recipient-name"],
+            $_POST["street-num"],
+            $_POST["zip-code"],
+            $_POST["city"],
+            $_POST["country"],
+            $_POST["state"]
+        );
+    }
 ?>
